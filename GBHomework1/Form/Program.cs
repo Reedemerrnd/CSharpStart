f1185cb8170bec8717db59ea72db14e6492c6e4f
Roman Avdeev <reedemer@yandex.ru>
2020-12-19 12:26:01 +0300

    Домашнее задание 1

---
+++ GBHomework1/Form/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region
+/* Авдеев Роман
+    Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). 
+    В результате вся информация выводится в одну строчку:
+    а) используя  склеивание;
+	б) используя форматированный вывод;
+	в) используя вывод со знаком $.
+*/
+#endregion
+namespace Form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст");
+            int age = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите рост");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите вес");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            //вариант а
+            Console.WriteLine("Привет, " + name + " " + surname + "! Вам "
+                                + age + " лет(года). Ваш рост: " + height
+                                + " и Ваш вес: " + weight + "."
+                                );
+            //вариант б
+            Console.WriteLine(string.Format("Привет, " + name + " "
+                                            + surname + "! Вам "
+                                            + age + " лет(года). Ваш рост: "
+                                            + height + " и Ваш вес: " + weight + ".")
+                                            );
+            //вариант в
+            Console.WriteLine($"Привет, {name} {surname}! Вам {age} лет(года). Ваш рост: {height} и Ваш вес: {weight}.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GBHomework1/Number5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Авдеев Роман
+    а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+    б) *Сделать задание, только вывод организовать в центре экрана.
+    в) **Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y).
+*/
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Роман";
+            string surname = "Авдеев";
+            string city = "Ростов-На-Дону";
+            //Вариант а)
+            Console.WriteLine("Привет! Меня зовут {0} {1}. Я из города {2}.", name, surname, city);
+            Console.ReadLine();
+
+            //Вариант б)
+            Console.Clear();
+            string message = "Привет! Меня зовут " + name + " " + surname + ". Я из города " + city + ".";
+            int centerX = Console.WindowWidth / 2 - message.Length / 2;
+            int centerY = Console.WindowHeight / 2 - 1;
+            Console.SetCursorPosition(centerX, centerY);
+            Console.WriteLine(message);
+            Console.ReadLine();
+            Console.Clear();
+
+            //Вариант в)
+            ConsoleMethods.Print(message, "center");
+            ConsoleMethods.Pause();
+
+        }
+    }
+}
