db13ea0ff99cd557f8de4380930770d5a8cdc41e
Roman Avdeev <reedemer@yandex.ru>
2020-12-21 21:57:17 +0300

    Домашнее задание 2

---
+++ Homework2/GoodNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**Авдеев Роман
+ * Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000. 
+ * «Хорошим» называется число, которое делится на сумму своих цифр. 
+ * Реализовать подсчёт времени выполнения программы, используя структуру DateTime.
+ */
+namespace GoodNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            int count = 0;
+            for (int i = 1; i <= 1000000000; i++)
+            {
+                if (i % DigitsSum(i) == 0)
+                    count++;
+            }
+            
+            Console.WriteLine(count);
+            Console.WriteLine(DateTime.Now-start);
+
+            Console.ReadKey();
+        }
+        static int DigitsSum(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum+=number%10;
+                number = number / 10;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Homework2/MinOfThree/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Авдеев Роман
+//Написать метод, возвращающий минимальное из трёх чисел.
+namespace MinOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(MinOfThree(1,2,3));
+            Console.WriteLine(MinOfThree(10, 2, 30));
+            Console.WriteLine(MinOfThree(100, 20, 3));
+
+            Console.ReadKey();
+        }
+        static double MinOfThree(double a, double b, double c)
+        {
+            if (a < b && a < c)
+                return a;
+            else if (b < a && b < c)
+                return b;
+            else return c;
+        }
+    }
+}
