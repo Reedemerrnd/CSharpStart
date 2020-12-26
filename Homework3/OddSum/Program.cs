b86765fe3d25ef21eb6bd140819dfa7697b291da
Roman Avdeev <reedemer@yandex.ru>
2020-12-26 15:55:20 +0300

    Обработка исключений

--- Homework3/OddSum/Program.cs
+++ Homework3/OddSum/Program.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //Авдеев Роман
-//С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел
+/*С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел
+б) Добавить обработку исключительных ситуаций на то, что могут быть введены некорректные данные. 
+При возникновении ошибки вывести сообщение. Напишите соответствующую функцию;
+*/
 namespace PositiveOddSumm
 {
     class Program
@@ -17,9 +20,13 @@ namespace PositiveOddSumm
             do
             {
                 Console.WriteLine("Enter number");
-                Int32.TryParse(Console.ReadLine(), out n);
-                if (n > 0 && n % 2 != 0)
-                    sum += n;
+                if (Int32.TryParse(Console.ReadLine(), out n))
+                {
+                    if (n > 0 && n % 2 != 0)
+                        sum += n;
+                }
+                else n=1;
+
             }
 
             while (n != 0);
