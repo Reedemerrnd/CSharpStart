a6af6e306ab0b578d4853befe7ac939b966e2738
Roman Avdeev <reedemer@yandex.ru>
2021-01-19 14:45:12 +0300

    Homework 6 complete

--- Homework6/Students/Program.cs
+++ Homework6/Students/Program.cs
@@ -6,12 +6,12 @@ using System.Text;
 using System.Threading.Tasks;
 /*Avdeev Roman
  * Переделать программу «Пример использования коллекций» для решения следующих задач:
-а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
-б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся (частотный массив);
-в) отсортировать список по возрасту студента;
-г) *отсортировать список по курсу и возрасту студента;
+а) Подсчитать количество студентов учащихся на 5 и 6 курсах;+
+б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся (частотный массив);+
+в) отсортировать список по возрасту студента;+
+г) *отсортировать список по курсу и возрасту студента;+
 д) разработать единый метод подсчета количества студентов по различным параметрам
-выбора с помощью делегата и методов предикатов.
+выбора с помощью делегата и методов предикатов.+
 */
 namespace Students
 {
@@ -25,8 +25,7 @@ namespace Students
         public string department;
         public int group;
         public string city;
-        int age;
-        // Создаем конструктор
+        public int age;
         public Student(string firstName, string lastName, string university, string faculty, string department, int course, int age, int group, string city)
         {
             this.lastName = lastName;
@@ -39,45 +38,97 @@ namespace Students
             this.group = group;
             this.city = city;
         }
+        public override string ToString()
+        {
+            return $"{firstName} {lastName},city {city}, age: {age}, course: {course}";
+        }
     }
+
     class Program
     {
-        static int MyDelegat(Student st1, Student st2)          // Создаем метод для сравнения для экземпляров
+        static int ByAge(Student st1, Student st2)          
         {
 
-            return String.Compare(st1.firstName, st2.firstName);          // Сравниваем две строки
+            if (st1.age < st2.age) return -1;
+            else if (st1.age > st2.age) return 1;
+            else return 0;
+        }
+        // сортировка по курсу и возрасту
+        static int ByCousreAndAge(Student st1, Student st2)
+        {
+            if (st1.course < st2.course) return -1;
+            else if (st1.course == st2.course)
+            {
+                if (st1.age < st2.age) return -1;
+                else if (st1.age > st2.age) return 1;
+                else return 0;
+            }
+            else return 1;
+        }
+        // счетчики и метод подсчета через делегат
+        static bool AgeMatch (Student st1)
+        {
+            return st1.age < 18;
+        }
+        static bool CityMatch (Student st)
+        {
+            return st.city == "Москва";
+        }
+        static int CountItems(List<Student> st,Predicate<Student> p)
+        {
+            var r = st.FindAll(p).Count();
+            return r;
         }
         static void Main(string[] args)
         {
-            int bakalavr = 0;
-            int magistr = 0;
-            List<Student> list = new List<Student>();                             // Создаем список студентов
+            int courseFive = 0;
+            int courseSix = 0;
+            List<Student> list = new List<Student>();
+            int[] dic = new int[7];
             DateTime dt = DateTime.Now;
-            StreamReader sr = new StreamReader("students_6.csv");
+            StreamReader sr = new StreamReader("students_6.csv",Encoding.UTF8);
             while (!sr.EndOfStream)
             {
                 try
                 {
                     string[] s = sr.ReadLine().Split(';');
-                    // Добавляем в список новый экземпляр класса Student
                     list.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
-                    // Одновременно подсчитываем количество бакалавров и магистров
-                    if (int.Parse(s[5]) < 5) bakalavr++; else magistr++;
+                    int age = int.Parse(s[6]);
+                    int course = int.Parse(s[5]);
+                    //считаем студентов на 5 и 6 курсах
+                    if (course == 5) courseFive++; 
+                    else if (course == 6) courseSix++;
+                    //частотный массив
+                    if (age >= 18 && age <= 20) dic[course]++; 
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
                     Console.WriteLine("Ошибка!ESC - прекратить выполнение программы");
-                    // Выход из Main
+
                     if (Console.ReadKey().Key == ConsoleKey.Escape) return;
                 }
             }
             sr.Close();
-            list.Sort(new Comparison<Student>(MyDelegat));
             Console.WriteLine("Всего студентов:" + list.Count);
-            Console.WriteLine("Магистров:{0}", magistr);
-            Console.WriteLine("Бакалавров:{0}", bakalavr);
-            foreach (var v in list) Console.WriteLine(v.firstName);
+            Console.WriteLine("5 курс:{0}", courseFive);
+            Console.WriteLine("6 курс:{0}", courseSix);
+            Console.WriteLine("частотный по курсам 18-20 лет");
+            for (int i = 1; i <7; i++)
+            {
+                Console.WriteLine($"{i} курс: {dic[i]}");
+            }
+            Console.WriteLine("sort by age");
+            list.Sort(ByAge);
+            foreach (var v in list) Console.WriteLine(v);
+            Console.WriteLine("sort by course and age");
+            list.Sort(ByCousreAndAge);
+            foreach (var v in list) Console.WriteLine(v);
+            Console.WriteLine();
+            Console.WriteLine("count age<18");
+            Console.WriteLine(CountItems(list,AgeMatch));
+            Console.WriteLine("count city = Москва");
+            Console.WriteLine(CountItems(list,CityMatch));
             Console.WriteLine(DateTime.Now - dt);
             Console.ReadKey();
         }
